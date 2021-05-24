982ccb01f2cdc87081d6d1d4ea1bd5c758171183
ambitious-caller-8426 <anhcdang@gmail.com>
2021-05-24 18:43:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,7 @@ def monopoly_with_third_tier
       :names => {
         :reading_railroad => {},
         :pennsylvania_railroad => {},
-        :b_ando_railroad => {},
+        :b_and_o_railroad => {},
         :shortline_railroad => {}
       }
     }
@@ -65,7 +65,7 @@ def monopoly_with_fourth_tier
         :pennsylvania_railroad => {
           :mortgage_value => 200
         },
-        :b_ando_railroad => {
+        :b_and_o_railroad => {
           :mortgage_value => 400
         },
         :shortline_railroad => {
